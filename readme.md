237bd1290e070d4cde9535ef36365af5f31e3732
Paritosh Raj <paritosh@hasura.io>
2021-07-02 00:51:37 +0530

    API added

--- readme.md
+++ readme.md
@@ -1,17 +1,17 @@
-# Notekeeper using Haskell
+# Webapp and API using Haskell
 
-This project aims at building a web app [notekeeper](https://haskell-at-work.com/episodes/2018-04-09-your-first-web-application-with-spock.html) using Haskell. The project is built using [cabal](https://www.haskell.org/cabal/#install-upgrade) and [Spock](https://www.spock.li/), so make sure you have cabal installed before using this (Spock will be installed during the process). Lets dive into the installation process (I am assumiung you have already installed cabal)
+This project aims at building a web app and API using Haskell. The project is built using [cabal](https://www.haskell.org/cabal/#install-upgrade) and [Spock](https://www.spock.li/), so make sure you have cabal installed before using this (Spock will be installed during the process). Lets dive into the installation process (I am assumiung you have already installed cabal)
 
 ## Installation
 The process is pretty simple, you need to follow the following steps to get started:
 ### Steps:
 1. Clone the repo using the following command:
 ``` bash
-git clone git@github.com:paritosh-08/notekeeper.git
+git clone git@github.com:paritosh-08/personkeeper.git
 ```
 2. Install using the following command:
 ``` bash
-cd notekeeper
+cd personkeeper
 cabal update
 cabal new-install
 ```
@@ -19,6 +19,15 @@ cabal new-install
 ## Run the program
 Run the following command to run the program:
 ``` bash
-cabal new-run :notekeeper
+cabal new-run :personkeeper
 ```
-Now open your browser and go to http://localhost:8080/
+Now open your browser and go to http://localhost:3000/ for the webapp, here you can view the people in the database and can add new person into the database using the form.
+
+For API, go to http://localhost:3000/api, here you will get a json output of all the people in the database, you can get specific people on the endpoint: http://localhost:3000/api/[ID]. Also you can add new people by a post request on the API url:
+``` bash
+curl -H "Content-Type: application/json" -d '{ "name": "New Person", "age": 31 }' localhost:3000/api
+```
+
+## References
+1. https://www.spock.li/tutorials/rest-api
+2. https://haskell-at-work.com/episodes/2018-04-09-your-first-web-application-with-spock.html
--- src/Main.hs
+++ src/Main.hs
@@ -1,55 +1,113 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric     #-}
+
+{-# LANGUAGE EmptyDataDecls             #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE DataKinds #-}
 
 module Main where
 
-import Web.Spock
-import Web.Spock.Config
-import Web.Spock.Lucid (lucid)
-import Lucid
-import Data.Text (Text)
-import Data.IORef
-import Control.Monad (forM_)
-import Control.Monad.IO.Class (liftIO)
-import Data.Semigroup ((<>))
+import           Web.Spock
+import           Web.Spock.Config
+import           Web.Spock.Lucid        (lucid)
+import           Lucid
+import           Data.Text              (Text, pack, unpack)
+import           Data.IORef
+import           Control.Monad          (forM_)
+import           Control.Monad.IO.Class (liftIO)
+import           Data.Semigroup         ((<>))
+import           Data.Aeson             hiding (json)
+import           GHC.Generics
 
-type Server a = SpockM () () ServerState a
+import           Control.Monad.Logger    (LoggingT, runStdoutLoggingT)
+import           Database.Persist        hiding (get)
+import qualified Database.Persist        as P
+import           Database.Persist.Sqlite hiding (get)
+import           Database.Persist.TH
 
-newtype ServerState = ServerState { notes :: IORef [Note] }
-data Note = Note { author :: Text, contents :: Text }
 
-app :: Server ()
-app = do
+type Server = SpockM SqlBackend () () ()
+
+type ApiAction a = SpockAction SqlBackend () () a
+
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+Person json -- The json keyword will make Persistent generate sensible ToJSON and FromJSON instances for us.
+  name Text
+  age Int
+  deriving Show
+|]
+
+runSQL :: (HasSpock m, SpockConn m ~ SqlBackend)  => SqlPersistT (LoggingT IO) a -> m a
+runSQL action = runQuery $ \conn -> runStdoutLoggingT $ runSqlConn action conn
+
+errorJson :: Int -> Text -> ApiAction ()
+errorJson code message =
+  json $
+    object
+    [ "result" .= String "failure"
+    , "error" .= object ["code" .= code, "message" .= message]
+    ]
+
+app :: Server
+app = do    
     get root $ do
-        notes' <- getState >>= (liftIO . readIORef . notes)
+        allPeople <- runSQL $ selectList [] [Asc PersonId]
         lucid $ do
-            h1_ "Notes"
-            ul_ $ forM_ notes' $ \note -> li_ $ do
-                toHtml (author note)
-                ": "
-                toHtml (contents note)
-            h2_ "New Note"
+            h1_ "People"
+            ul_ $ forM_ allPeople $ \people -> li_ $ do
+                "Name : "
+                toHtml (unpack (personName (entityVal people)))
+                ", Age : "
+                toHtml (show (personAge (entityVal people)))
+                " years"
+            h2_ "New Person"
             form_ [method_ "post"] $ do
                 label_ $ do
-                    "Author: "
-                    input_ [name_ "author"]
+                    "Name: "
+                    input_ [name_ "name"]
                 label_ $ do
-                    "Contents: "
-                    textarea_ [name_ "contents"] ""
-                input_ [type_ "submit", value_ "Add Note"]
+                    "Age: "
+                    input_ [name_ "age"]
+                input_ [type_ "submit", value_ "Add Person"]
     post root $ do
-        author <- param' "author"
-        contents <- param' "contents"
-        notesRef <- notes <$> getState
-        liftIO $ atomicModifyIORef' notesRef $ \notes ->
-            (notes <> [Note author contents], ())
+        namePerson <- param' "name"
+        agePerson <- param' "age"
+        let newPerson = 
+                Person {personName = namePerson, personAge = agePerson}
+            in runSQL $ insert newPerson
         redirect "/"
+
+    get "api" $ do
+        allPeople <- runSQL $ selectList [] [Asc PersonId]
+        json allPeople
+    post "api" $ do
+        maybePerson <- jsonBody :: ApiAction (Maybe Person)
+        case maybePerson of
+            Nothing -> errorJson 1 "Failed to parse request body as Person"
+            Just thePerson -> do
+                newId <- runSQL $ insert thePerson
+                json $ object ["result" .= String "success", "id" .= newId]
+    get ("api" <//> var) $ \personId -> do
+        maybePerson <- runSQL $ P.get personId :: ApiAction (Maybe Person)
+        case maybePerson of
+            Nothing -> errorJson 2 "Could not find a person with matching id"
+            Just thePerson -> json thePerson
 
 
 main :: IO ()
 main = do
-    st <- ServerState <$>
-        newIORef [ Note "Alice" "Must not forget to walk the dog."
-                , Note "Bob" "Must. Eat. Pizza!"
-                ]
-    cfg <- defaultSpockCfg () PCNoDatabase st
-    runSpock 8080 (spock cfg app)
+    pool <- runStdoutLoggingT $ createSqlitePool "api.db" 5
+    
+    cfg <- defaultSpockCfg () (PCPool pool) ()
+    runStdoutLoggingT $ runSqlPool (do runMigration migrateAll) pool
+    runSpock 3000 (spock cfg app)
