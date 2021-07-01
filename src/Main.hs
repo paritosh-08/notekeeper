ee37d5d0badbff4ec029e8e4a652929fe7cb0fb4
Paritosh Raj <paritosh@hasura.io>
2021-07-01 10:20:16 +0530

    init

---
+++ src/Main.hs
@@ -0,0 +1,55 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import Web.Spock
+import Web.Spock.Config
+import Web.Spock.Lucid (lucid)
+import Lucid
+import Data.Text (Text)
+import Data.IORef
+import Control.Monad (forM_)
+import Control.Monad.IO.Class (liftIO)
+import Data.Semigroup ((<>))
+
+type Server a = SpockM () () ServerState a
+
+newtype ServerState = ServerState { notes :: IORef [Note] }
+data Note = Note { author :: Text, contents :: Text }
+
+app :: Server ()
+app = do
+    get root $ do
+        notes' <- getState >>= (liftIO . readIORef . notes)
+        lucid $ do
+            h1_ "Notes"
+            ul_ $ forM_ notes' $ \note -> li_ $ do
+                toHtml (author note)
+                ": "
+                toHtml (contents note)
+            h2_ "New Note"
+            form_ [method_ "post"] $ do
+                label_ $ do
+                    "Author: "
+                    input_ [name_ "author"]
+                label_ $ do
+                    "Contents: "
+                    textarea_ [name_ "contents"] ""
+                input_ [type_ "submit", value_ "Add Note"]
+    post root $ do
+        author <- param' "author"
+        contents <- param' "contents"
+        notesRef <- notes <$> getState
+        liftIO $ atomicModifyIORef' notesRef $ \notes ->
+            (notes <> [Note author contents], ())
+        redirect "/"
+
+
+main :: IO ()
+main = do
+    st <- ServerState <$>
+        newIORef [ Note "Alice" "Must not forget to walk the dog."
+                , Note "Bob" "Must. Eat. Pizza!"
+                ]
+    cfg <- defaultSpockCfg () PCNoDatabase st
+    runSpock 8080 (spock cfg app)
